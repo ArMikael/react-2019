0fe6d435ef729d73e3f59bc4e50f6e15610e2fd2
Michael Treser <armikael2@gmail.com>
2019-09-10 17:41:56 +0300

    Added styles form todo list items.

--- src/todos/Todo.js
+++ src/todos/Todo.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const styles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: '4px 20px',
+        padding: '0.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '3px' 
+    },
+
+    button: {
+        display: 'inline-block',
+        width: '20px',
+        height: '20px',
+        backgroundColor: 'beige',
+        marginLeft: '4px',
+        textAlign: 'center',
+        border: 'none'
+    }
+};
+
 function Todo({ todo, index }) {
     return(
-        <li>
-            <span>{index + 1}. </span>
-            {todo.title}
+        <li style={styles.li}>
+            <span>
+                <input type="checkbox" />
+                <span>{index + 1}. </span>
+                {todo.title}
+            </span>
+
+            <button style={styles.button}>&times;</button>
         </li>
     );
 }
 
-Todo.PropTypes = {
+Todo.propTypes = {
   todo: PropTypes.object.isRequired,
   index: PropTypes.number
 };
